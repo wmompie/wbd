136ddd5043530cd2d98de2902ceb6814a0d23494
Will Mompie <wmompie@gardco.com>
2019-05-07 14:18:13 -0400

    checking redirects in netlify

---
+++ build/precache-manifest.f19d8bfa9a7120e1414befc672013042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fa7fc0458a9cd75fbcdf7b8a445172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23340f106ce134b08bd7",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "1ea3a155dc8052f6f3a2",
+    "url": "/static/js/2.3dc10679.chunk.js"
+  },
+  {
+    "revision": "23340f106ce134b08bd7",
+    "url": "/static/js/main.650960f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
